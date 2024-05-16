3dd9ceb838bc1cefb24cf86f220aa781177de06d
Mustapha Mohammed <mustaphamohammed0705@gmail.com>
2024-05-16 17:53:33 +0000

    Create an Instance of BaseModel from a Dictionary using Key-Word Arguments

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,20 @@ from datetime import datetime
 
 class BaseModel:
     """The base class that the other classes inherit from"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Defines public instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Prints class name, the id, and available attributes as key-value pairs"""
@@ -25,3 +34,4 @@ class BaseModel:
         dic_copy["created_at"] = self.created_at.isoformat()
         dic_copy["updated_at"] = self.updated_at.isoformat()
         return dic_copy
+    
