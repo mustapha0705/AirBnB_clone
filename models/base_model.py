38a0cb7f7313b5a07e906da8c237bc61246e9893
Mustapha Mohammed <mustaphamohammed0705@gmail.com>
2024-05-17 15:15:03 +0000

    Removed test code in base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -49,15 +49,3 @@ class BaseModel:
         #Returns the dictionary
         return dic_copy
     
-all_objs = models.storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
-
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
